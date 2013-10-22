1ff2d1d5cf9faf3b14fe3ed9581aeb69dd83567d
Carlos Rosão <shemahmforash@gmail.com>
2013-10-22 12:13:47 +0100

    Module updated to cpan format

--- bin/gsa.pl
+++ bin/gsa.pl
@@ -8,30 +8,22 @@ use Data::Dumper;
 use DateTime;
 use Date::Parse;
 
-=for
-my $epoch    = Date::Parse::str2time("2013-08-12 11:09:43");
-my $datetime = DateTime->from_epoch(
-    'epoch'     => $epoch,
-    'time_zone' => 'local',
-);
-
-#die Dumper $datetime->strftime('%a, %d %b %y %T %Z');
-
-die Dumper $datetime->strftime('%a, %d %b %Y %H:%M:%S %z');
-
-#RFC822 (Mon, 15 Nov 2004 04:58:08 GMT)
-=cut
-
-my $gsa = XML::GSA->new('base_url' => 'http://www.optimus.pt', 'type' => 'full');
-
+my $gsa = XML::GSA->new('base_url' => 'http://icdif.com');
 my $xml = $gsa->create(
-    [   {   'records' => [
-                {   'url'           => '/particulares',
-                    'mimetype'      => 'text/plain',
-                    'abc' => 'abc'
+    [   {   'action'  => 'add',
+            'records' => [
+                {   'url'      => '/particulares',
+                    'mimetype' => 'text/plain',
+                    'action'   => 'delete',
                 },
-            ]
-        }
+                {   'url'      => '/empresas',
+                    'mimetype' => 'text/plain',
+                    'metadata' => [
+                        { 'name' => 'og:title', 'content' => 'Empresas' },
+                    ],
+                }
+            ],
+        },
     ]
 );
 
@@ -70,6 +62,6 @@ my $xml = $gsa->create(
 );
 =cut
 
-print $gsa->xml();
+print $xml;
 
 1;
